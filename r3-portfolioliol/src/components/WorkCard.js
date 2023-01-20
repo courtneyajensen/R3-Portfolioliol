b09af931b06da23c4f725d16107334371a3b610d
Courtney Jensen <courtneyajensen14@gmail.com>
2023-01-19 20:42:54 -0700

    routes and such

---
+++ r3-portfolioliol/src/components/WorkCard.js
@@ -0,0 +1,19 @@
+import "../components/WorkCardStyles.css";
+import React from 'react'
+
+const WorkCard = (props) => {
+    return (
+        <div className="project-card">
+            <img src={props.imgsrc} alt="projectPreview" />
+            <h2 className="project-title">{props.title}</h2>
+            <div className="pro-details">
+                <p>{props.text}</p>
+                <div className="pro-btns"></div>
+                <a href={props.view} className="btn" target="_blank" rel="noreferrer">View</a>
+                <a href={props.source} className="btn btn-light" target="_blank" rel="noreferrer">Source</a>
+            </div>
+        </div>
+    )
+}
+
+export default WorkCard;
---
+++ r3-portfolioliol/src/components/WorkCardData.js
@@ -0,0 +1,29 @@
+import upkeep from "../assets/UpKeep.png";
+import notetaker from "../assets/NoteTaker.png";
+import codingquiz from "../assets/CodingQuiz.jpg";
+
+const ProjectCardData = [
+    {
+        imgsrc: upkeep,
+        title: "Up Keep",
+        text: "Up Keep helps you organize and perform home maintenance by season.",
+        view: "https://github.com/courtneyajensen/UP-Keep",
+        source: "https://github.com/courtneyajensen/UP-Keep"
+    },
+    {
+        imgsrc: notetaker,
+        title: "Note Taker",
+        text: "Jot down notes as you need, set reminders too!",
+        view: "https://not3-tak3r.herokuapp.com/notes",
+        source: "https://github.com/courtneyajensen/Note-Taker"
+    },
+    {
+        imgsrc: codingquiz,
+        title: "Coding Quiz",
+        text: "Test your knowledge with these fun coding related questions!",
+        view: "https://courtneyajensen.github.io/Code-Quiz/",
+        source: "https://github.com/courtneyajensen/Code-Quiz"
+    }
+]
+
+export default ProjectCardData;
